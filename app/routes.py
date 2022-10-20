6f24b5d8d8131d9f0d43db9edd46fcf2d689541d
June Valentino <vintydesign@gmail.com>
2022-10-20 16:38:22 -0700

    added books blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 hello_world_bp = Blueprint("hello_world", __name__)
 
@@ -15,6 +15,7 @@ def say_hello_json():
         "hobbies": ["Video Games", "Art", "Animation"]
     }
 
+
 @hello_world_bp.route("/fixed-endpoint-with-fixed-server-code")
 def broken_endpoint():
     response_body = {
@@ -25,3 +26,27 @@ def broken_endpoint():
     new_hobby = "Surfing"
     response_body["hobbies"].append(new_hobby)
     return response_body
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "The Prince and the Dressmaker", "A story about the seamstress who makes dresses for her prince."),
+    Book(3, "Lost at Sea", "The story of a girl, Riley, who finds herself on a roadtrip with a car- full of strangers.")
+] 
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+@books_bp.route("", methods = ['GET'])
+def handle_books():
+    books_result = []
+    for book in books:
+        books_result.append({
+            'id': book.id,
+            'title': book.title,
+            'description': book.description
+        })
+    return jsonify(books_result)
