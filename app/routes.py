346563d4ef2d434c3532aadb7c23fc5fc12b9070
June Valentino <vintydesign@gmail.com>
2022-10-20 17:09:01 -0700

    added 404 result

--- app/routes.py
+++ app/routes.py
@@ -50,3 +50,16 @@ def handle_books():
             'description': book.description
         })
     return jsonify(books_result)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description,
+            }
+
+    return {"message":f"book {book_id} not found"}, 404
